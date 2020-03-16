5ff4a3ad4803e91a2aefd53025073afcd0d27ece
Reed Jones <reed.jones@pderas.com>
2020-03-15 18:17:47 -0600

    font optimizing

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -4,8 +4,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="theme-color" content="#4299e1">
 <meta name="Description" content="Phase - A blazing fastish Server Rendered Vue/Laravel integration meta framework.">
-<link rel="preload" as="font" href="/fonts/subset-Againts.woff2" type="font/woff2" crossorigin="anonymous">
-<link rel="preload" as="font" href="/fonts/subset-Againts.woff" type="font/woff" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link rel="preload" as="font" href="/fonts/subset-Againts.woff2" type="font/woff2" crossorigin="anonymous">
 <link rel="preload" href="https://fonts.googleapis.com/css?family=Inter&display=swap" as="style">
 <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
