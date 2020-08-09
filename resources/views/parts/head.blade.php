2eb720e5b354d998ab8703b763115e131c4d80a3
Reed Jones <reedjones@reedjones.com>
2020-08-08 23:17:24 -0600

    update meta

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -3,8 +3,17 @@
 <title>{{ config('app.name') }}</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="theme-color" content="#4299e1">
+<meta name="monetization" content="$ilp.uphold.com/frxp2rWaKQAA">
 <meta name="Description" content="Phase - A blazing fastish Server Rendered Vue/Laravel integration meta framework.">
 <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
 <link rel="preload" as="font" href="/fonts/subset-Againts.woff2" type="font/woff2" crossorigin="anonymous">
 <link rel="preload" href="https://fonts.googleapis.com/css?family=Inter&display=swap" as="style">
 <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-22308864-7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-22308864-7');
+</script>
