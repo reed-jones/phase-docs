d7ab297dc0c7d1dedd50ae71ad55c3b43ca2fa12
Reed Jones <reedjones@reedjones.com>
2020-08-08 19:17:07 -0600

    wip

--- api/assets.php
+++ api/assets.php
@@ -8,15 +8,15 @@ $file = $_GET['file'] ?? null;
 
 switch($_GET['type']) {
     case 'css':
-        header("Content-type: text/css; charset: UTF-8");
-        echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+        header("Content-type: text/css;");
+        return readfile(__DIR__ . '/../public/css/' . basename($_GET['file']));
         break;
     case 'js':
-        header('Content-Type: application/javascript; charset: UTF-8');
-        echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+        header('Content-Type: application/javascript;');
+        return readfile(__DIR__ . '/../public/js/' . basename($_GET['file']));
         break;
     case 'fonts':
         header('Content-Type: font/woff2;');
-        echo require __DIR__ . '/../public/fonts/' . basename($_GET['file']);
+        return readfile(__DIR__ . '/../public/fonts/' . basename($_GET['file']));
         break;
 }
